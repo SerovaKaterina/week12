9cb17ff5c11d32760023c51a168193694216b8e6
SerovaKaterina <k.serova.k@yandex.ru>
2023-05-29 19:12:43 +0300

    add changes

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,3 @@
-function seeResult (){
-    changeName ();
-    changeImage ();
-    changeComment ();
-}
-
 const fullName = document.querySelector(".form1");
 const yourFoto = document.querySelector(".form2");
 const yourComment = document.querySelector(".form3");
@@ -13,18 +7,19 @@ const resultName = document.querySelector(".fullname");
 const resultComment = document.querySelector(".yourcomment");
 
 function changeName() {
-    let capital = fullName[0].toUpperCase();
-    let rest = fullName.slice(1).toLowerCase();
-    resultName = capital + rest;
+  let capital = fullName.value.charAt(0).toUpperCase();
+  let rest = fullName.value.slice(1).toLowerCase();
+  resultName.innerHTML = capital + rest;
 }
-console.log(changeName);
-
-
 function changeComment() {
-    resultComment = yourComment.replace(/(xxx|viagra)/gi , '***');
+  resultComment.innerHTML = yourComment.value.replace(/(xxx|viagra)/gi, "***");
 }
-console.log(changeComment);
-
 function changeImage() {
-    document.querySelector ('.yourfoto').src = yourFoto.value;
+  document.querySelector(".yourfoto").src = yourFoto.value;
+}
+
+function seeResult() {
+  changeName();
+  changeImage();
+  changeComment();
 }
